d4352af60104f41492639998dc902a00f6350367
gromolga <gromolga.work@gmail.com>
2020-12-01 18:57:51 +0200

    initial commit

---
+++ A_LEVEL_EXERSISE_5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace A_LEVEL_EXERSISE_5
+{
+    /// <summary>
+    /// Start point of app.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Start of app.
+        /// </summary>
+        /// <param name="args">args added via console.</param>
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
